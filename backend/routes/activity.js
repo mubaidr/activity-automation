056920410bb707b6e19a8d8a4e43b147cb7d83db
Muhammad Ubaid Raza <mubaidr@gmail.com>
2018-01-11 21:36:29 +0500

    implement activty api

--- backend/routes/activity.js
+++ backend/routes/activity.js
@@ -2,7 +2,27 @@ const express = require('express')
 
 const router = express.Router()
 
-router.get('/', (req, res) => {
+router.get('/all', (req, res, next) => {
+  const db = req.app.get('db')
+  const login = req.account
+
+  if (login.accountType.description === 'user') {
+    res.send(401)
+    return
+  }
+
+  db.activity
+    .findAll({
+      include: [db.login],
+      raw: true
+    })
+    .then(activities => {
+      res.send(activities)
+    })
+    .catch(next)
+})
+
+router.get('/', (req, res, next) => {
   const db = req.app.get('db')
   const login = req.account
 
@@ -11,17 +31,26 @@ router.get('/', (req, res) => {
       where: {
         loginId: login.id
       },
-      include: [db.login]
+      include: [db.login],
+      raw: true
     })
     .then(activities => {
-      console.log(activities)
-
-      res.send('activity')
+      res.send(activities)
     })
+    .catch(next)
 })
 
-router.post('/', (req, res) => {
-  res.send(`activity: ${req.body}`)
+router.post('/', (req, res, next) => {
+  const db = req.app.get('db')
+  const login = req.account
+  const activity = db.activity.build({ ...req.body, loginId: login.id })
+
+  activity
+    .create()
+    .then(() => {
+      res.sendStatus(200)
+    })
+    .catch(next)
 })
 
 module.exports = router
